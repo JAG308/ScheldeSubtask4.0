59cf0074ebdd81b010b7874462833d29bf537c83
Jesus Alonso Guerra <jesus.alonso.guerra@vliz.be>
2023-12-20 13:55:32 +0100

    Outliers per Parameter ID

---
+++ Outliers_ParaID1074.R
@@ -0,0 +1,82 @@
+
+
+library(usethis)
+library(devtools)
+devtools::install_github("scheldemonitor/scheldemonitoR")
+library(scheldemonitoR)
+
+fytoplankton_outlier_analysis <- function(dataset_id, start_year, end_year, source) {
+  # Import data
+  fytoplankton_data <- importBioticData(dataset_id, start = start_year, end = end_year, source = source)
+  tbl1 <- fytoplankton_data
+  
+  # Function to find outliers in a column
+  find_outliers <- function(parameter, col_name) {
+    Q1 <- quantile(parameter, 0.25, na.rm = TRUE)
+    Q3 <- quantile(parameter, 0.75, na.rm = TRUE)
+    IQR <- Q3 - Q1
+    lower_bound <- Q1 - 1.5 * IQR
+    upper_bound <- Q3 + 1.5 * IQR
+    
+    # Find the indices of outliers
+    outlier_indices <- which(parameter < lower_bound | parameter > upper_bound)
+    
+    if (length(outlier_indices) > 0) {
+      # Create a data frame with the results
+      result_df <- data.frame(ID = outlier_indices, ColumnName = col_name)
+      return(result_df)
+    } else {
+      return(NULL)
+    }
+  }
+  
+  # Function to find outliers in numeric columns and handle non-numeric columns
+  find_outliers_function <- function(tbl1) {
+    # Initialize empty data frames to store results
+    numeric_outliers <- data.frame(ID = integer(0), ColumnName = character(0))
+    non_numeric_columns <- character(0)
+    
+    # Function to check if a column contains numeric values
+    is_numeric_column <- function(column) {
+      all(is.numeric(column))
+    }
+    
+    # Loop through each column (parameter) and find outliers
+    for (col_name in colnames(tbl1)) {
+      # Skip columns to ignore
+      column_values <- tbl1[[col_name]]
+      
+      # Check if the column contains numeric values
+      if (is_numeric_column(column_values)) {
+        outliers_for_column <- find_outliers(column_values, col_name)
+        if (!is.null(outliers_for_column)) {
+          numeric_outliers <- rbind(numeric_outliers, outliers_for_column)
+        }
+      } else {
+        # Handle non-numeric values
+        non_numeric_columns <- c(non_numeric_columns, col_name)
+      }
+    }
+    
+    # Return the results
+    return(list(numeric_outliers = numeric_outliers, non_numeric_columns = non_numeric_columns))
+  }
+  
+  # Call the outlier analysis function
+  results <- find_outliers_function(tbl1)
+  
+  # Access the numeric outliers and non-numeric columns
+  numeric_outliers <- results$numeric_outliers
+  non_numeric_columns <- results$non_numeric_columns
+  
+  # Return the results
+  return(list(numeric_outliers = numeric_outliers, non_numeric_columns = non_numeric_columns))
+}
+
+# Example usage:
+ result <- fytoplankton_outlier_analysis(1074, start_year = 2013, end_year = 2015, source = "imis")
+
+
+ table(numeric_outliers$ColumnName)
+ tail(numeric_outliers, n=500)
+ 
