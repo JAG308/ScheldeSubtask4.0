4db487c873bcdd03adfc0b3e930ac3068be81102
Jesus Alonso Guerra <jesus.alonso.guerra@vliz.be>
2024-02-08 16:26:41 +0100

    Add outliers source

--- MajorScript.R
+++ MajorScript.R
@@ -1,3 +1,7 @@
+### Main code that withdraws all others functions and allows Quality Control of specific parameters.
+
+################# HEATMAPS ################
+
 source("https://raw.githubusercontent.com/JAG308/ScheldeSubtask4.0/main/heatmaps.R")
 
 # Function heatmapsQC with a specified parameter Id
@@ -12,9 +16,25 @@ run_heatmap_with_parameter <- function(parameter_id) {
 }
 
 # Set parameter Id
-parameter_id <- 461 # You can change this to any parameter Id you want to visualize
+parameter_id <- 4939 # You can change this to any parameter Id you want to visualize
 heatmap_figures <- run_heatmap_with_parameter(parameter_id)
 
 heatmap_figures
+
+
+############### OUTLIERS ###############
+
+source("https://raw.githubusercontent.com/JAG308/ScheldeSubtask4.0/main/Outliers_ParaID1074.R")
+
+# Example usage:
+parameter_id <- 13449  # You can change this to any parameter Id you want to analyze
+start_year <- 2018
+end_year <- 2021
+
+
+result <- run_outliers_analysis(parameter_id, start_year, end_year)
+
+print(result)
+
 
 
