a97db99abf22521a7112a2f537b1b25c6939a873
Jesus Alonso Guerra <jesus.alonso.guerra@vliz.be>
2024-02-08 15:41:59 +0100

    Major script that sources the rest

---
+++ MajorScript.R
@@ -0,0 +1,20 @@
+source("https://raw.githubusercontent.com/JAG308/ScheldeSubtask4.0/main/heatmaps.R")
+
+# Define a function to run heatmapsQC with a specified parameter Id
+run_heatmap_with_parameter <- function(parameter_id) {
+  # Call heatmapsQC function with the specified parameter Id
+  all_data <- importAbioticData(parameter_id, start = 2018, end = 2021)
+  
+  # plot available data
+  all_availability_figures <- heatmapDataAvailability(all_data, "high tide", "year", "station")
+  
+  return(all_availability_figures)
+}
+
+# Example usage:
+parameter_id <- 461 # You can change this to any parameter Id you want to visualize
+heatmap_figures <- run_heatmap_with_parameter(parameter_id)
+
+heatmap_figures
+
+
